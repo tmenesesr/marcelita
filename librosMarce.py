8a8f20a4b06488b0ce531df78e7b6abdb926ced1
Tirso Meneses Rojas <115514929+tmenesesr@users.noreply.github.com>
2024-02-22 15:38:25 -0300

    Agrega página ancha

--- librosMarce.py
+++ librosMarce.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import openpyxl
 
-
+st.set_page_config(layout="wide")
 libros = pd.read_excel("miBiblioteca.xlsx")
 st.title("Libros de Marcela Vaga")
 st.subheader("La más linda")
